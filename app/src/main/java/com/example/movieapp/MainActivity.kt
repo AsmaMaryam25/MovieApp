c9e3ca8997dc08556d4501cb6c6afd97e27469b2
AsmaMaryam25 <s230716@dtu.dk>
2024-10-29 02:12:10 +0100

    Navigation bar done, color combination will change in next meeting

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -7,11 +7,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.movieapp.ui.theme.MovieAppTheme
+import androidx.navigation.compose.rememberNavController
+import com.example.movieapp.components.navigation.BottomNavigationBar
+import com.example.movieapp.components.navigation.BottomNavItem
+import com.example.movieapp.components.navigation.NavigationGraph
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.List
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +26,48 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MovieAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                val navController = rememberNavController()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    bottomBar = {
+                        BottomNavigationBar(
+                            items = listOf(
+                                BottomNavItem(
+                                    name = "Home",
+                                    route = "home",
+                                    icon = Icons.Outlined.Home
+                                ),
+                                BottomNavItem(
+                                    name = "Search",
+                                    route = "search",
+                                    icon = Icons.Outlined.Search
+                                ),
+                                BottomNavItem(
+                                    name = "Favourite",
+                                    route = "favourite",
+                                    icon = Icons.Outlined.Favorite
+                                ),
+                                BottomNavItem(
+                                    name = "Watchlist",
+                                    route = "watchlist",
+                                    icon = Icons.AutoMirrored.Outlined.List
+                                ),
+                                BottomNavItem(
+                                    name = "Settings",
+                                    route = "settings",
+                                    icon = Icons.Outlined.Settings
+                                )
+                            ),
+                            navController = navController
+                        )
+                    }
+                ) { innerPadding ->
+                    NavigationGraph(
+                        navController = navController,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MovieAppTheme {
-        Greeting("Android")
     }
 }
