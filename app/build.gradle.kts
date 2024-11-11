16b3fb370b0ec85c5c6c542d724ac1b73d2e251f
TuranTalayhan <tura0015@gmail.com>
2024-11-11 08:55:09 +0100

    added viewmodel plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
 dependencies {
     implementation("androidx.compose.material:material-icons-extended:1.0.0-alpha08")
     implementation("com.google.accompanist:accompanist-pager:0.24.13-rc")
-
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
     implementation("androidx.activity:activity-compose:1.9.3")
