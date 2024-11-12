5eef2094ad6759642ff098d7fc14d6e309b8d980
AsmaMaryam25 <s230716@dtu.dk>
2024-11-12 17:45:52 +0100

    searchbar is done

--- app/src/main/java/com/example/movieapp/screens/SearchScreen.kt
+++ app/src/main/java/com/example/movieapp/screens/SearchScreen.kt
@@ -8,8 +8,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.SearchBar
+import com.example.movieapp.components.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -25,18 +29,29 @@ import dk.shape.dtu.navigation.R
 fun SearchScreen(modifier: Modifier = Modifier) {
     val posterWidth = 170.dp
     //TODO add search bar
-    LazyColumn(
-        modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    )
-    {
-        items(10) {
-            Row {
-                CreateSearchPoster(posterWidth)
-                CreateSearchPoster(posterWidth)
+    val searchQuery = remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        SearchBar(
+            searchQuery = searchQuery,
+            onSearchQueryChange = { query ->
+                // Handle search
             }
-        }
-    }
+        )
+
+        LazyColumn(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            items(10) {
+                Row {
+                    CreateSearchPoster(posterWidth)
+                    CreateSearchPoster(posterWidth)
+                }
+            }
+          }
+       }
 }
 
 @Composable
