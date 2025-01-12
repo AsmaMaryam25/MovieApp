ee0404967295d7fc5e8ae48ef113cb1bdc37bb17
Daniel Lerche <s235095@dtu.dk>
2025-01-12 18:08:01 +0100

    fix: Made the util function is more correct

--- app/src/main/java/com/example/blackbeard/utils/TimeUtils.kt
+++ app/src/main/java/com/example/blackbeard/utils/TimeUtils.kt
@@ -3,8 +3,13 @@ package com.example.blackbeard.utils
 object TimeUtils {
 
     fun convertMinutesToHoursAndMinutes(totalMinutes: Int): String {
+        if(totalMinutes < 0) return "0 m"
         val hours = totalMinutes / 60
         val minutes = totalMinutes % 60
+
+        if(minutes == 0 && hours == 0) {
+            return "0 m"
+        }
 
         if(hours < 1) {
             return "$minutes m"
