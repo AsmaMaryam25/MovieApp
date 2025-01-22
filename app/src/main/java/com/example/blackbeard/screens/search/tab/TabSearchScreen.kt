c1a437fd083a93ab209d8f908e0a4e0cf406a1e5
TuranTalayhan <tura0015@gmail.com>
2025-01-22 09:30:42 +0100

    fix: recent search now searches when pressed

--- app/src/main/java/com/example/blackbeard/screens/search/tab/TabSearchScreen.kt
+++ app/src/main/java/com/example/blackbeard/screens/search/tab/TabSearchScreen.kt
@@ -55,7 +55,7 @@ fun TabSearchScreen(
                         }
                     },
                     selectedCategories = tabSearchViewModel.selectedCategories,
-                    onAdvancedSearch = onSearchClicked
+                    onSearch = onSearchClicked
                 )
             }
         }
